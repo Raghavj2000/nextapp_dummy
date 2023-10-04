556ed970cb4661b56960c72013535d776f1c1e02
RaghavendraJ <RaghavendraJ@ziniosedge.com>
2023-10-04 17:22:54 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      "https://img.freepik.com/free-photo/young-adult-enjoying-yoga-nature_23-2149573175.jpg?w=900&t=st=1696419806~exp=1696420406~hmac=a0663d2a936f6dd68e8d973174fb47425d6865ca5c12850ea9c91272277ecc38",
+    ], // Add the domain(s) you need
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
